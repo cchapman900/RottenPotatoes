5781db3899471fbf3942be97853c8c3d037ffe41
Chris <chrischapman900@gmail.com>
2014-08-31 23:23:00 -0400

    Add config folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '9d8899f6411db9e94ce698d023aad969a7075367ac4dfaee49e657759c73baa7d53c38a9df501b5ec9b89ec3e5d0bebb052972585b7c7ee8790444ea652d9365'
